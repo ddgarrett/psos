989ed7594dc77e28eaa1b40ac2ab3b1080bcc6e7
ddgarrett <doug@anetsc.com>
2022-07-28 16:41:10 -0700

    Remove Ref to self_device

--- svc_log.py
+++ svc_log.py
@@ -13,7 +13,7 @@ class ModuleService(PsosService):
     
     def __init__(self, parms):
         super().__init__(parms)
-        self._pub_topic = parms.get_parm("pub_log",self._device+"/log")
+        self._pub_topic = parms.get_parm("pub_log","log")
         
     async def log_msg(self,name,msg):
         
