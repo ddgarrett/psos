3770e7f7eb24b13837e279cdcd06b16160a2e4d5
ddgarrett <doug@anetsc.com>
2022-09-27 21:06:39 -0700

    Support Micro SD Card Module

--- base/svc_sd_card.py
+++ base/svc_sd_card.py
@@ -27,6 +27,8 @@ from psos_svc import PsosService
 import machine
 import sdcard
 import os
+import queue
+import gc
 
 
 
@@ -36,9 +38,21 @@ class ModuleService(PsosService):
     def __init__(self, parms):
         super().__init__(parms)
 
-        # TODO: allow parms to specify details such as pins and mount point name
-        spi = machine.SPI(1, baudrate=100000, phase=0, polarity=0, sck=machine.Pin(14), mosi=machine.Pin(15), miso=machine.Pin(2)) 
-        sd = sdcard.SDCard(spi, machine.Pin(13))
+        slot = self.get_parm("slot",1)
+        sck  = self.get_parm("sck",14)
+        mosi = self.get_parm("mosi",15)
+        miso = self.get_parm("miso",2)
+        cs   = self.get_parm("cs",13)
+        baud = self.get_parm("baud",1_000_000)
+        
+        # spi = machine.SPI(1, baudrate=100000, phase=0, polarity=0, sck=machine.Pin(14), mosi=machine.Pin(15), miso=machine.Pin(2)) 
+        # sd = sdcard.SDCard(spi, machine.Pin(13))
+        
+        # spi = machine.SPI(2, baudrate=100000, phase=0, polarity=0, sck=machine.Pin(18), mosi=machine.Pin(23), miso=machine.Pin(19)) 
+        # sd = sdcard.SDCard(spi, machine.Pin(5))
+        
+        spi = machine.SPI(slot, baudrate=baud, phase=0, polarity=0, sck=machine.Pin(sck), mosi=machine.Pin(mosi), miso=machine.Pin(miso)) 
+        sd = sdcard.SDCard(spi, machine.Pin(cs))
         
         self._mount_pt = self.get_parm('mount_pt','/sd')
         os.mount(sd,self._mount_pt)
@@ -46,3 +60,83 @@ class ModuleService(PsosService):
     def unmount(self):
         os.umount(self._mount_pt)
         
+    async def run(self):
+        
+        upd = self.get_parm("subscr_upd",None)
+        if  upd == None:
+            return
+        
+        q = queue.Queue()
+        mqtt = self.get_mqtt()
+        
+        await mqtt.subscribe(upd,q)
+        
+        while True:
+            data = await q.get()
+            await self.send_data(mqtt,data)
+            
+            
+    # send data via MQTT after receiving a update request
+    async def send_data(self,mqtt,data):
+        
+        if len(data) >= 3 and data[2] == "test":
+            await self.test_sd()
+        
+        msg = {
+            'disk_free' : self.df(),
+            'mem_free'  : self.free() }
+        
+        await mqtt.publish(self.get_parm("pub_upd"),msg)
+
+    # test SD card read write 
+    async def test_sd(self):
+        await self.log('******************************')
+        await self.log('Root:\t{}'.format(os.listdir()))
+        
+        os.chdir(self._mount_pt)
+        await self.log('SD Card:\t{}'.format(os.listdir()))
+        
+        await self.log('Write to SD Card')
+        f = open('sample2.txt', 'w')
+        f.write('Some text for sample 2\n')
+        f.close()
+        
+        await self.log('Read from SD Card')
+        f = open('sample2.txt')
+        await self.log(f.read())
+        f.close()
+         
+        await self.log('Append to file')
+        f = open('sample2.txt', 'a')
+        f.write('More text for sample 2\n')
+        f.close()
+        
+        await self.log('Read from SD Card')
+        f = open('sample2.txt')
+        await self.log(f.read())
+        f.close()
+
+        await self.log('Delete Sample File')
+        os.remove('sample2.txt')
+
+        await self.log("test complete")
+        os.chdir('/')
+        await self.log('******************************')
+        
+    # disk free space
+    def df(self):
+        s = os.statvfs(self._mount_pt)
+        blk_size = s[0]
+        total_mb = (s[2] * blk_size) / 1048576
+        free_mb  = (s[3] * blk_size) / 1048576
+        pct = free_mb/total_mb*100
+        return '{0:,.2f}MB ({1:.0f}%)'.format(free_mb, pct)
+        # return ('DFr {0:.2f}MB {1:.0f}%'.format(free_mb, pct))
+
+    def free(self):
+        gc.collect() # run garbage collector before checking memory 
+        F = gc.mem_free()
+        A = gc.mem_alloc()
+        T = F+A
+        P = '{0:.0f}%'.format(F/T*100)
+        return '{0:,} ({1})'.format(F,P)
