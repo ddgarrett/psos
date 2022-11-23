f56925fbf7604db4ce20db8df89108884d996190
ddgarrett <doug@anetsc.com>
2022-11-23 15:02:17 -0800

    Move Menu to Separate JSON File

--- base/svc_menu.py
+++ base/svc_menu.py
@@ -19,6 +19,7 @@ from psos_svc import PsosService
 from psos_parms import PsosParms
 import uasyncio
 import queue
+import ujson
 
 from svc_msg import SvcMsg
 
@@ -36,22 +37,30 @@ class ModuleService(PsosService):
         
         self.sub = parms.get_parm("sub")
         self.lcd = parms.get_parm("lcd")
-        self.items = parms.get_parm("items")
+        self.menu = parms.get_parm("menu")
+        # self.items = parms.get_parm("items")
         
         self.q = queue.Queue()
         self.in_menu = False
         self.item_idx = 0
+        self.state = []
         
-        self.quick = self.get_parm("quick",{})
+        # self.quick = self.get_parm("quick",{})
         
         # builtin commands defined at the endof this module
         self.cmd_bi = {
                 "exit": self.cmd_exit,
                 "pub" : self.cmd_pub,
-                "msg" : self.cmd_msg
+                "msg" : self.cmd_msg,
+                "submenu": self.cmd_submenu
             }
 
     async def run(self):
+        
+        self.menu = self.parse_menu(self.menu)
+        main = self.menu["main"]
+        self.quick = main["quick"]
+        self.items = main["menu"]
         
         mqtt = self.get_mqtt()
         await mqtt.subscribe(self.sub,self.q)
@@ -64,7 +73,15 @@ class ModuleService(PsosService):
             msg.load_subscr(data)
             await self.execute(mqtt,msg)
             
-            
+    def parse_menu(self,fn):
+        # read the paramter file
+        with open(fn) as f:
+            parms = ujson.load(f)
+            f.close()
+            return parms
+        
+        return None
+        
     # execute a given command received via MQTT
     async def execute(self,mqtt,msg):
 
@@ -83,7 +100,8 @@ class ModuleService(PsosService):
             elif mp == m_down:
                 await self.scroll_menu(mp)
             elif mp == m_exit:
-                await self.exit_menu()
+                # go to parent menu if it exists
+                await self.pop_menu() 
             elif mp == m_enter:
                 await self.select_item()
             else:
@@ -98,13 +116,37 @@ class ModuleService(PsosService):
         self.lcd.set_timeout(0)
 
     async def exit_menu(self,msg=None):
-        self.in_menu = False
+        
+        # Are we running a submenu?
+        # Just redisplay previous menu before submenu
+        if len(self.state) > 0:
+            state = self.state[0]
+            self.items    = state["items"]
+            self.item_idx = state["idx"]        
+        
+        # exit menu service
+        self.in_menu  = False
+        self.item_idx = 0
         self.unlock_lcd()
         self.lcd.set_timeout(self.save_timeout)
         if msg == None:
             msg = " "
             
         self.update_lcd(msg)
+    
+    # If in a submenu, pop the state of the previous menu
+    # and display it.
+    # If not in submenu, exit the menu
+    async def pop_menu(self,msg=None):
+        
+        if len(self.state) > 0:
+            state = self.state.pop()
+            self.items    = state["items"]
+            self.item_idx = state["idx"]
+            self.display_menu()
+            return
+        
+        await self.exit_menu(msg=msg)
 
     async def scroll_menu(self,cmd):
         if cmd == m_down:
@@ -170,9 +212,10 @@ class ModuleService(PsosService):
     # limited run then exit.
     async def exec_cmd(self,exit_msg,parms):
         
-        # add exit message and command name to parms
+        # add exit message, command name and menu object to parms
         parms["exit_msg"]  = exit_msg
         parms["name"]      = parms["cmd"]
+        parms["menu"]      = self
         
         # create the psos parms for the command
         psos_parms = PsosParms(parms,self._parms._defaults)
@@ -212,7 +255,21 @@ class ModuleService(PsosService):
         msg = exit_msg
         if "msg" in parms:
             msg = parms["msg"]
-        self.update_lcd(msg)        
+        self.update_lcd(msg)
+        
+    async def cmd_submenu(self,exit_msg,parms):
+        
+        if "name" in parms:
+            submenu_name = parms["name"]
+            submenu  = self.menu[submenu_name]
+    
+            # change my menu items to the submenu menu items
+            # then display that new menu
+            self.state.append({"items":self.items,"idx":self.item_idx})
+            self.item_idx = 0
+            self.items = submenu["menu"]
+            self.display_menu()
+        
 
     #########################################
    
