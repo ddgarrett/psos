3cf7fc8cae855afde1fbc0937c3dd8aa5e7c67bf
ddgarrett <doug@anetsc.com>
2023-01-02 05:11:11 -0800

    Simplify Build Manifest Process

--- build_manifest.py
+++ build_manifest.py
@@ -16,7 +16,7 @@
       - Delete files which are not in the repo manifest.json file
       - Add files which are in the repo manifest but not in the local copy
 
-    Once that is completed, the remote device replaces the local copy
+    During that process the remote device replaces the local copy
     of the manifest.json file with the repo version.
 '''
 
@@ -44,6 +44,7 @@ parms = {
 def get_github_manifest(dir):
     parms["dir"] = dir
     uri = "{uri}/{user}/{repo}/contents/{dir}".format(**parms)
+    print("... reading git dir:",uri)
     r = requests.get(uri,headers=headers)
     c = r.json()
     r.close()
@@ -62,7 +63,7 @@ def load_json(fn):
 def save_json(fn,o):
     with open(fn, "w") as f:
         # json.dump(o, f, sort_keys=True, indent=4)
-        json.dump(o, f, sort_keys=True, indent=0)
+        json.dump(o, f, indent=1)
         
 # convert a list to a dictionary
 def cnv_list_to_dict(lst,key):
@@ -72,85 +73,45 @@ def cnv_list_to_dict(lst,key):
         
     return r        
 
-# synch local directory with git
-def sync_dir(local,git):
+# return the list of files and file sha for a given directory
+# returns the list of files 
+def rebuild_dir(name):
+    print("rebuild directory",name)
+    files = []
 
-    if local["sha"] == git["sha"]:
-        print("no changes in directory",local["name"])
-        return 
+    # get github manifest for directory
+    git = get_github_manifest(name)
     
-    # update local sha
-    local["sha"] = git["sha"]
-    directory = local["name"]
-    
-    print("update directory",directory)
-    
-    git_mani = get_github_manifest(directory)
-    git_mani = cnv_list_to_dict(git_mani,"name")
-    
-    for local_file in local["files"].copy():
-        fn = local_file["name"]
-        if not fn in git_mani:
-            local["files"].remove(local_file)
-            print("...removed",fn)
-        else:
-            git = git_mani[fn]
-            if local_file["sha"] != git["sha"]:
-                print("...update","/"+directory+"/"+fn)
-                local_file["sha"] = git["sha"]
-                
-            # remove from the git manifest to detect new files
-            del git_mani[fn]
-            
-    # At this point, all git manifest items that are in
-    # the local file have been deleted from the
-    # git manifest dictionary. Therefore all remaining
-    # git manifest items are new to local file system
-        
-    for git_obj in git_mani.values():
-        # ignore any non-file entries
-        if git_obj["type"] != "file":
-            continue
-        
-        name = git_obj["name"]
-        sha  = git_obj["sha"]
-        new_entry = {"name":name,"sha":sha}
-        
-        local["files"].append(new_entry)
-        print("...add "+"/"+directory+"/"+name)
-        # save_git_file("/"+directory+"/"+name)
-            
+    for obj in git:
+        if obj["type"] == "file":
+            e = {"file": obj["name"],
+                 "sha" : obj["sha"] }
+            files.append(e)
+
+    print("... {} files in dir {}".format(len(files),name))
+    return files
 
 
-def update(fn):
-    print("checking repo changes")
-    local = load_json(fn)
-    
+# rebuild the manifest using the initial manifest
+# defined in filename (fn)
+def rebuild_manifest(fn):
+    print("rebuilding manifest")
+    manifest = load_json(fn)
+
     # get github manifest for root directory
     # and convert to a dictionary
     git = get_github_manifest("")
     git = cnv_list_to_dict(git,"name")
 
-    for local_obj in local["obj"]:
-        
-        # error if local object no longer in git
-        git_obj = git[local_obj["name"]]
-        if "files" in local_obj:
-            sync_dir(local_obj,git_obj)
-        else:
-            sha = git_obj["sha"]
-            fn  = git_obj["name"]
-
-            if local_obj["sha"] != sha:
-                print("update",fn)
-                local_obj["sha"] = sha
+    for obj in manifest["obj"]:
+        obj["sha"] = git[obj["name"]]["sha"]
+        if "files" in obj:
+            obj["files"] = rebuild_dir(obj["name"])
             
-    # save_json(fn,local)
-    return local
+    return manifest
 
-fn = "manifest.json"
-new_manifest = update(fn)
 
-if new_manifest != None :
-    save_json(fn,new_manifest)
+# new_manifest = update(fn)
+manifest = rebuild_manifest("manifest_initial.json")
+save_json("manifest.json",manifest)
     
